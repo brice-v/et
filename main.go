d8322db36bd21ab5dfb9c20862ae7b86ba2c3f8f
brice <bricevadnais@yahoo.com>
2026-06-16 22:21:30 -0400

    et: basic viewer and style printed even when no content

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strings"
 
 	"github.com/gdamore/tcell/v3"
 )
@@ -14,17 +15,39 @@ import (
 const logFileName = "et.log"
 const version = "0.0.1"
 
-func draw(s tcell.Screen, cfg *config.Config, filename string) {
-	style := tcell.StyleDefault.Background(cfg.Colors.Background.Color).Foreground(cfg.Colors.Foreground.Color)
-	statusStyle := style.Background(cfg.Colors.StatusBar.Color)
-	s.Clear()
-	w, h := s.Size()
+func drawLine(s tcell.Screen, baseStyle tcell.Style, w, lineNumberOnScreen int, line string) {
+	lineRunes := []rune(line)
+	lineLen := len(lineRunes)
 	for x := range w {
-		s.SetContent(x, h-1, ' ', nil, statusStyle)
+		if x >= lineLen || lineLen == 0 {
+			s.SetContent(x, lineNumberOnScreen, ' ', nil, baseStyle)
+		} else {
+			s.SetContent(x, lineNumberOnScreen, lineRunes[x], nil, baseStyle)
+		}
+	}
+}
+
+func drawContent(s tcell.Screen, baseStyle tcell.Style, w, h int, fileContent string) {
+	lines := strings.Split(fileContent, "\n")
+	numLines := len(lines)
+	for i := range h {
+		l := ""
+		if i < numLines {
+			l = lines[i]
+		}
+		drawLine(s, baseStyle, w, i, l)
+	}
+}
+
+func drawStatusBar(s tcell.Screen, cfg *config.Config, baseStyle tcell.Style, w, h int, fileName string) {
+	statusStyle := baseStyle.Background(cfg.Colors.StatusBar.Color)
+	statusBarH := h - 1
+	for x := range w {
+		s.SetContent(x, statusBarH, ' ', nil, statusStyle)
 	}
 	quitKeyBindsString := cfg.GetQuitKeyBindingsAsStr()
-	fnameStr := filename
-	if filename == "" {
+	fnameStr := fileName
+	if fileName == "" {
 		fnameStr = "<new file>"
 	}
 	statusMsg := fmt.Sprintf(" et - %s | %s to quit", fnameStr, quitKeyBindsString)
@@ -32,8 +55,16 @@ func draw(s tcell.Screen, cfg *config.Config, filename string) {
 		if i >= w {
 			break
 		}
-		s.SetContent(i, h-1, ch, nil, statusStyle)
+		s.SetContent(i, statusBarH, ch, nil, statusStyle)
 	}
+}
+
+func draw(s tcell.Screen, cfg *config.Config, fileName, fileContent string) {
+	style := tcell.StyleDefault.Background(cfg.Colors.Background.Color).Foreground(cfg.Colors.Foreground.Color)
+	s.Clear()
+	w, h := s.Size()
+	drawContent(s, style, w, h, fileContent)
+	drawStatusBar(s, cfg, style, w, h, fileName)
 	s.Show()
 }
 
@@ -47,7 +78,7 @@ func IsKeyAny(key tcell.Key, keyAsRune string, keys []config.Key) bool {
 }
 
 func main() {
-	filename := flag.String("f", "", "file to open")
+	fileName := flag.String("f", "", "file to open")
 	showHelp := flag.Bool("help", false, "show help")
 	showVersion := flag.Bool("version", false, "show version")
 	showVersion2 := flag.Bool("v", false, "show version")
@@ -66,8 +97,8 @@ func main() {
 		fmt.Printf("et v%s\n", version)
 		os.Exit(0)
 	}
-	if *filename == "" && len(os.Args) > 1 {
-		*filename = os.Args[1]
+	if *fileName == "" && len(os.Args) > 1 {
+		*fileName = os.Args[1]
 	}
 
 	screen, err := tcell.NewScreen()
@@ -82,12 +113,21 @@ func main() {
 	defer screen.Fini()
 
 	cfg := config.NewDefault()
-	draw(screen, cfg, *filename)
+	fileContent := ""
+	if *fileName != "" {
+		data, err := os.ReadFile(*fileName)
+		if err != nil {
+			slog.Warn("could not read file", "err", err)
+		}
+		fileContent = string(data)
+		slog.Info("fileContent", "content", fileContent)
+	}
+	draw(screen, cfg, *fileName, fileContent)
 
 	for ev := range screen.EventQ() {
 		switch e := ev.(type) {
 		case *tcell.EventResize:
-			draw(screen, cfg, *filename)
+			draw(screen, cfg, *fileName, fileContent)
 		case *tcell.EventKey:
 			keyAsRune := ""
 			if e.Key() == tcell.KeyRune {
