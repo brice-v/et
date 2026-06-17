63437ba964be2bd0a43b9342db4a7f2dea5e6332
brice <bricevadnais@yahoo.com>
2026-06-16 20:39:31 -0400

    log: add logger

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/gdamore/tcell/v3"
 )
+
+const logFileName = "et.log"
+const version = "0.0.1"
 
 func draw(s tcell.Screen, cfg *config.Config, filename string) {
 	style := tcell.StyleDefault.Background(cfg.Colors.Background.Color).Foreground(cfg.Colors.Foreground.Color)
@@ -31,9 +35,9 @@ func draw(s tcell.Screen, cfg *config.Config, filename string) {
 	s.Show()
 }
 
-func IsKeyAny(key tcell.Key, keys []config.Key) bool {
+func IsKeyAny(key tcell.Key, keyAsRune string, keys []config.Key) bool {
 	for _, k := range keys {
-		if key == k.Key {
+		if key == k.Key || (keyAsRune != "" && keyAsRune == k.String()) {
 			return true
 		}
 	}
@@ -43,19 +47,32 @@ func IsKeyAny(key tcell.Key, keys []config.Key) bool {
 func main() {
 	filename := flag.String("f", "", "file to open")
 	showHelp := flag.Bool("help", false, "show help")
+	showVersion := flag.Bool("version", false, "show version")
+	showVersion2 := flag.Bool("v", false, "show version")
 	flag.Parse()
+	f, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed to create log file: %s", err.Error())
+	}
+	jsonSlog := slog.New(slog.NewJSONHandler(f, nil))
+	slog.SetDefault(jsonSlog)
 
 	if *showHelp {
 		flag.Usage()
+		os.Exit(0)
+	} else if *showVersion || *showVersion2 {
+		fmt.Printf("et v%s\n", version)
 		os.Exit(0)
 	}
 
 	screen, err := tcell.NewScreen()
 	if err != nil {
-		log.Fatalf("creating screen: %v", err)
+		slog.Error("creating screen", "err", err)
+		os.Exit(1)
 	}
 	if err := screen.Init(); err != nil {
-		log.Fatalf("initializing screen: %v", err)
+		slog.Error("initializing screen", "err", err)
+		os.Exit(1)
 	}
 	defer screen.Fini()
 
@@ -67,7 +84,11 @@ func main() {
 		case *tcell.EventResize:
 			draw(screen, cfg, *filename)
 		case *tcell.EventKey:
-			if IsKeyAny(e.Key(), cfg.KeyBindings.Quit) {
+			keyAsRune := ""
+			if e.Key() == tcell.KeyRune {
+				keyAsRune = e.Str()
+			}
+			if IsKeyAny(e.Key(), keyAsRune, cfg.KeyBindings.Quit) {
 				return
 			}
 		}
